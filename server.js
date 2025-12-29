ddfaf687c03f3e31c464e010eb20777f6e4abfbf
davigrossis <davi.grossi.souza@gmail.com>
2025-12-29 00:17:17 -0300

    add: initial configs and server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//public forder to store assets
+app.use(express.static(__dirname + '/public'));
+
+// routes for app
+app.get('/', function (req, res) {
+  res.render('pad');
+});
+
+//listen to port 3000 (for localhost) or the port defined
+var port = process.env.PORT || 3000;
+app.listen(port);
+
